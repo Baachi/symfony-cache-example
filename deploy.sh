f764d9bf83e031e6d920da422661fc9ce07a3521
markus <HGq7w5k>
2014-12-05 15:10:23 +0100

    Add dev to composer

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-php composer.phar install --prefer-dist --no-dev --no-scripts
+php composer.phar install --prefer-dist --no-scripts
 php app/console -e=prod cache:clear
 php app/console -e=dev cache:clear
