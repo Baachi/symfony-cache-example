b7cf043c82454c4632ebfabc4de802b09fa976ef
markus <HGq7w5k>
2014-12-05 14:45:54 +0100

    Add cache header

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,11 +4,13 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Cache(smaxage="3600")
      */
     public function indexAction()
     {
